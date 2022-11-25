c1cf7420a7ae31fefd2e1e0c861dbca8caba2719
Michael Rybka <merybka@gmail.com>
2022-11-25 16:19:54 -0500

    Add AuthEndpoint to Service

--- service.go
+++ service.go
@@ -8,7 +8,8 @@ import (
 )
 
 type Service struct {
-	DataDir string `json:"dataDir"`
+	DataDir      string `json:"dataDir"`
+	AuthEndpoint string `json:"authEndpoint"`
 }
 
 func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
