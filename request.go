7552c07eeff29d845ad32fd22e1cab0bbc4882d5
Michael Rybka <merybka@gmail.com>
2022-11-30 17:35:33 -0500

    Change request json representation

--- request.go
+++ request.go
@@ -5,5 +5,5 @@ import "github.com/library-development/go-auth"
 type Request struct {
 	Auth    *auth.Credentials `json:"auth"`
 	Command string            `json:"command"`
-	Input   map[string]string `json:"args"`
+	Input   map[string]string `json:"input"`
 }
