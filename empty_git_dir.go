09d9c26d94f6d5d06e5675267314d8336002a41b
Michael Rybka <merybka@gmail.com>
2022-12-04 12:26:16 -0500

    Implement emptyGitDir

--- empty_git_dir.go
+++ empty_git_dir.go
@@ -1,3 +1,18 @@
 package schemacafe
 
-func emptyGitDir(dir string) error
+import (
+	"io/fs"
+	"os"
+)
+
+func emptyGitDir(dir string) error {
+	return fs.WalkDir(os.DirFS(dir), ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.Name() == ".git" {
+			return fs.SkipDir
+		}
+		return os.Remove(path)
+	})
+}
